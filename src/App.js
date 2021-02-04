eff7ec39d9e3e253514ce51e48b0d6cccc2974d7
Nancy Lee <nancy.lee@alexsolutions.com.au>
2021-02-05 10:35:13 +1100

    todo components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState }  from 'react';
 import './App.css';
+import data from "./data.json";
+
+import Header from "./components/Header";
+import ToDoList from "./components/ToDoList";
 
 function App() {
+  const [ toDoList, setToDoList ] = useState(data);
+
   return (
     <div className="App">
-      Hello World! A To Do List Will Go here!
+      <Header />
+      <ToDoList toDoList={toDoList}/>
     </div>
   );
 }
