79ab5def32299b3f0a70231a5e0970c33a6892b0
Nancy Lee <nancy.lee@alexsolutions.com.au>
2021-02-05 11:12:16 +1100

    add todo form

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import data from "./data.json";
 
 import Header from "./components/Header";
 import ToDoList from "./components/ToDoList";
+import ToDoForm from './components/ToDoForm';
 
 function App() {
   const [ toDoList, setToDoList ] = useState(data);
@@ -21,10 +22,17 @@ function App() {
     setToDoList(filtered);
   }
 
+  const addTask = (userInput) => {
+    let copy = [...toDoList];
+    copy = [...copy, { id: toDoList.length + 1, task: userInput, complete: false }];
+    setToDoList(copy);
+  }
+
   return (
     <div className="App">
       <Header />
-      <ToDoList toDoList={toDoList} handleToggle={handleToggle} handleFilter={handleFilter} />
+      <ToDoList toDoList={toDoList} handleToggle={handleToggle} handleFilter={handleFilter} addTask={addTask} />
+      <ToDoForm addTask={addTask}/>
     </div>
   );
 }
